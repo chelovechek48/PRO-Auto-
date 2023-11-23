abb14b19e6a4a8852ed1f2b765f07653f70003b5
Maksim Ivanyukhin <chelovechek.maks@mail.ru>
2023-11-23 04:37:54 +0300

    Added page header

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,21 @@
 import '@/styles/style';
+
+const burger = document.querySelector('.menu-burger');
+
+const headerCall = document.querySelector('.header__call');
+const rootHTML = document.querySelector(':root');
+const headerList = document.querySelector('.header__list');
+
+let burgerHasOpen = false;
+
+burger.addEventListener('click', () => {
+  const headerCallHeight = headerCall.offsetHeight;
+  rootHTML.style.setProperty('--header__callHeight', `${headerCallHeight}px`);
+
+  burger.classList.toggle('active');
+  headerList.classList.toggle('active');
+
+  burgerHasOpen = !burgerHasOpen;
+  const burgerLabelText = burgerHasOpen === false ? 'открыть меню' : 'закрыть меню';
+  burger.setAttribute('aria-label', burgerLabelText);
+});
